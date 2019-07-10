644935ba53103663ffb7e9fb3f08a47d44715131
mohamedkhairy <mohamed.khairy@orchtech.com>
2019-07-10 17:21:32 +0200

    end the design and router

---
+++ resources/js/components/homeComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- Main content -->
+  <div class="content">
+    <div class="container-fluid">
+      <div class="row">
+        <!-- /.col-md-6 -->
+        <div class="col-lg-12">
+          <div class="card card-primary card-outline">
+            <div class="card-header">
+              <h5 class="m-0 float-right d-none d-sm-inline">الصفحه الرئيسيه</h5>
+            </div>
+            <div class="card-body" style="float: right;margin-left: auto;">
+              <h6 class="card-title">بسم الله الرحمن الرحيم</h6>
+
+              <p class="card-text">لا اله الا الله محمد رسول الله</p>
+              <a href="#" class="btn btn-primary">اكشن</a>
+            </div>
+          </div>
+        </div>
+        <!-- /.col-md-6 -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </div>
+  <!-- /.content -->
+</template>
+
+<script>
+export default {
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,77 +1,102 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="ar" dir="rtl">
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+  <!-- Styles -->
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
-<body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+<body  class="hold-transition skin-blue sidebar-collapse">
+  <div class="wrapper" id="app">
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light border-bottom">
+      <!-- Left navbar links -->
+      <ul class="navbar-nav">
+        <!-- SEARCH FORM -->
+        <form class="form-inline ml-3">
+          <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar" type="search" placeholder="بحث" aria-label="Search">
+            <div class="input-group-append">
+              <button class="btn btn-navbar" type="submit">
+                <i class="fas fa-search"></i>
+              </button>
             </div>
-        </nav>
+          </div>
+        </form>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/home" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+                  <router-link to="/example" class="btn btn-primary border-left ">الصفحه الرئيسيه</router-link>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+          <a href="{{ route('logout') }}" class="btn btn-primary border-left " onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">تسجيل الخروج</a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+            @csrf
+          </form>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.navbar -->
+    
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+
+      <main class="py-4">
+        @yield('content')
+        <router-view></router-view>
+      </main>
+
     </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+      <!-- To the right -->
+      <div class="float-right d-none d-sm-inline">
+        Anything you want
+      </div>
+      <!-- Default to the left -->
+      <strong>Copyright &copy; 2019 <a href="https://know-me.000webhostapp.com">Mohamed Khairy</a>.</strong> All rights reserved.
+    </footer>
+  </div>
+  <!-- ./wrapper -->
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+
 </body>
+
 </html>
